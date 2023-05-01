0549df2f5992ebda1d65e984ee88be83c7cd9088
MOUKHAFI Anass <moukhafi.anass@gmail.com>
2023-05-01 17:57:02 +0000

    Add: DHT11 Humidity sensor

---
+++ 50_dht11_humidity_sensor/50_dht11_humidity_sensor.ino
@@ -0,0 +1,26 @@
+#include <DHT.h>
+#define Type DHT11
+int sensePin = 2;
+DHT sensor(sensePin, Type);
+float humidity;
+float tempC;
+float tempF;
+int pause = 2000;
+void setup() {
+  Serial.begin(9600);
+  sensor.begin();
+}
+
+void loop() {
+  humidity = sensor.readHumidity();
+  tempC = sensor.readTemperature();
+  tempF = sensor.readTemperature(true);
+  Serial.print("Humidity : ");
+  Serial.print(humidity);
+  Serial.print(", Temperature : ");
+  Serial.print(tempC);
+  Serial.print("C ");
+  Serial.print(tempF);
+  Serial.println("F.");
+  delay(pause);
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# A set of projects to learn Arduino
+### DHT humidity sensor :
+You will need the library for DHT sensor
